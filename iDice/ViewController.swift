6c8e98d36ad81426be51a6b60ed94046a21cc6c7
Nirbhay Agarwal <nirbhay.agarwal@gmail.com>
2018-06-02 08:23:25 +0530

    Initial Commit

---
+++ iDice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iDice
+//
+//  Created by Nirbhay Agarwal on 02/06/18.
+//  Copyright © 2018 NSRover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
